5a1835d6c8872e3e4b453bbc42a58e19ec5ea527
robiulsagor <robiul100.me@gmail.com>
2023-09-25 23:11:14 +0600

    Designing Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
 
 function App() {
 
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <Container>
+            <Content> hi there...</Content>
+        </Container>
     )
 }
+
+const Container = styled.section`
+    height: 100vh;
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+`
+
+const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: rebeccapurple;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 80px 40px;
+    /* margin-bottom: 10vw; */
+`
 
 export default Login
